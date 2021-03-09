4bd8dc88ab9c1e1b141428715c00e6acfb0995c6
diffi <73660258+diffiii@users.noreply.github.com>
2021-03-09 17:44:24 +0100

    Poke update

--- cogs/poke.py
+++ cogs/poke.py
@@ -11,12 +11,19 @@ class Poke(commands.Cog):
     @commands.has_permissions(manage_messages = True)
     async def poke(self, ctx, member: discord.Member):
         await ctx.message.delete()
+        if member.id == 593767655584956426: return
         current = member.voice.channel.id
         temp = 814840808296546314 if current == 770704857656328202 else 770704857656328202
         for i in range(3):
-            await member.move_to(self.client.get_channel(temp))
+            try:
+                await member.move_to(self.client.get_channel(temp))
+            except:
+                pass
             await asyncio.sleep(0.5)
-            await member.move_to(self.client.get_channel(current))
+            try:
+                await member.move_to(self.client.get_channel(current))
+            except:
+                pass
             await asyncio.sleep(0.5)
 
 
