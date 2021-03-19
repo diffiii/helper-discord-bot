9be3fec7f5185a307064abeb97e98fd35dc28af9
diffi <73660258+diffiii@users.noreply.github.com>
2021-03-19 22:31:32 +0100

    Stats fix

--- cogs/statistics.py
+++ cogs/statistics.py
@@ -39,13 +39,14 @@ class Statistics(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        custom_emojis = re.findall(r'<:\w*:\d*>', message.content)
-        custom_emojis = [int(e.split(':')[2].replace('>', '')) for e in custom_emojis]
-        custom_emojis = [discord.utils.get(await message.guild.fetch_emojis(), id = e) for e in custom_emojis]
-        stats = load_json('stats.json')
-        for emoji in custom_emojis:
-            stats[emoji.name] += 1
-        write_json('stats.json', stats)
+        if message.author != self.client:
+            custom_emojis = re.findall(r'<:\w*:\d*>', message.content)
+            custom_emojis = [int(e.split(':')[2].replace('>', '')) for e in custom_emojis]
+            custom_emojis = [discord.utils.get(await message.guild.fetch_emojis(), id = e) for e in custom_emojis]
+            stats = load_json('stats.json')
+            for emoji in custom_emojis:
+                stats[emoji.name] += 1
+            write_json('stats.json', stats)
 
 
 def setup(client):
